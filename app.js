cda19aebec3663836066db3601a2fc95e2cb3086
FaithC <faith1@Macbooks-MacBook-Pro-2.local>
2018-08-21 16:00:52 -0700

    App for Search Giphy @Make School

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+var exphbs  = require('express-handlebars');
+var http = require('http');
+var giphy = require('giphy-api')();
+
+
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    // console.log(req.query.term)
+//   var queryString = "funny cat";
+//   var term = encodeURIComponent(queryString);
+//   var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
+//   http.get(url, function(response) {
+//     response.setEncoding('utf8');
+//
+//     var body = '';
+//
+//     response.on('data', function(d) {
+//       body += d;
+//     });
+//
+//     response.on('end', function() {
+//       var parsed = JSON.parse(body);
+//       res.render('home', {gifs: parsed.data})
+//     });
+//   });
+giphy.search(req.query.term, function (err, response) {
+    res.render('home', {gifs: response.data})
+  });
+});
+
+
+
+
+app.listen(3000, function () {
+  console.log('Gif Search app listening on port localhost:3000!');
+});
