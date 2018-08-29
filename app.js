1786ca3d27a80db1a2b9b15b5b135892e83246ae
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2018-08-28 20:45:04 -0700

    refactored code in root route

--- app.js
+++ app.js
@@ -9,27 +9,28 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-    console.log(req.query.term)
-  var queryString = req.query.term;
-  var term = encodeURIComponent(queryString);
-  var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
-  http.get(url, function(response) {
-    response.setEncoding('utf8');
-
-    var body = '';
-
-    response.on('data', function(d) {
-      body += d;
+  //   console.log(req.query.term)
+  // var queryString = req.query.term;
+  // var term = encodeURIComponent(queryString);
+  // var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
+  // http.get(url, function(response) {
+  //   response.setEncoding('utf8');
+  //
+  //   var body = '';
+  //
+  //   response.on('data', function(d) {
+  //     body += d;
+  //     });
+  //
+  //   response.on('end', function() {
+  //     var parsed = JSON.parse(body);
+  //     res.render('home', {gifs: parsed.data})
+  //     });
+  //   });
+// Line 32 should say {res.data} not {response.data} like in tutorial
+    giphy.search(req.query.term, function (err, response) {
+        res.render('home', {gifs: res.data})
       });
-
-    response.on('end', function() {
-      var parsed = JSON.parse(body);
-      res.render('home', {gifs: parsed.data})
-      });
-    });
-    // giphy.search(req.query.term, function (err, response) {
-    //     res.render('home', {gifs: response.data})
-    //   });
 });
 
 app.listen(3000, function () {
